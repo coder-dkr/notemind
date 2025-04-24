4b006f170d102e14c86441973b1272d5cbc2faa4
Dhruv Roy <dhruvakr9001@gmail.com>
2025-04-24 17:14:18 +0530

    hojaa

--- app/dashboard/note/[id]/page.tsx
+++ app/dashboard/note/[id]/page.tsx
@@ -1,10 +1,11 @@
-// import { DashboardShell } from '@/components/dashboard/shell';
+import { DashboardShell } from '@/components/dashboard/shell';
 // import { NoteEditor } from '@/components/notes/note-editor';
 
-// export default function NotePage({ params }: pageprops) {
-//   return (
-//     <DashboardShell>
-//       <NoteEditor id={params.id} />
-//     </DashboardShell>
-//   );
-// }
+export default function NotePage() {
+  return (
+    <DashboardShell>
+      {/* <NoteEditor id={params.id} /> */}
+      <p>hey</p>
+    </DashboardShell>
+  );
+}
