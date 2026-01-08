971a21e6a59579906e88d08b9ed520d730458376
Dhruv Roy <dhruvakr9001@gmail.com>
2026-01-08 13:13:13 +0530

    extremely crucial fix

--- app/dashboard/note/[id]/page.tsx
+++ app/dashboard/note/[id]/page.tsx
@@ -2,7 +2,7 @@ import { DashboardShell } from '@/components/dashboard/shell';
 import { NoteEditor } from '@/components/notes/note-editor';
 
 export default async function NotePage({ params }: { params: { id: string } }) {
-  const { id } =  params;
+  const { id } =  await params;
   
   return (
     <DashboardShell>
